8b311436f9229d06006605c6f87ce48321aed37f
Min3CraftDud3 <hubeb93@gmail.com>
2014-07-28 15:54:28 -0400

    1

--- src/com/SinfulPixel/VCKOH/Tasks/GameManager.java
+++ src/com/SinfulPixel/VCKOH/Tasks/GameManager.java
@@ -1,6 +1,9 @@
 package com.SinfulPixel.VCKOH.Tasks;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -14,6 +17,30 @@ public class GameManager {
 	static ArrayList<Location> locations = new ArrayList<Location>();
 	
 	public GameManager(VCKOH plugin){GameManager.plugin = plugin;}
+	/*public static void cacheTimes(){
+		System.out.println("Caching run times.");
+		for(int i=0;i<500;i++){
+			if(plugin.getConfig().contains("Times."+i)){
+				time.add(plugin.getConfig().getString("Times."+i));	
+				System.out.println("Caching: "+plugin.getConfig().getString("Times"+i));
+			}
+		}
+		System.out.println("Caching run times...Complete.");
+	}
+	public static void cacheLoc(){
+		System.out.println("Caching locations.");
+		for(int i=0;i<500;i++){
+			if (plugin.getConfig().contains("Locations"+i)){
+				World w = Bukkit.getWorld("Locations"+i+".World");
+				double x = plugin.getConfig().getDouble("Locations"+i+".X");
+				double y = plugin.getConfig().getDouble("Locations"+i+".Y");
+				double z = plugin.getConfig().getDouble("Locations"+i+".Z");
+				locations.add(new Location(w,x,y,z));
+				System.out.println("Caching: "+w+x+y+z);
+				}
+			}
+		System.out.println("Caching locations...Complete.");
+	}*/
 	public static void cacheTimes(){
 		System.out.println("Caching run times.");
 		List<String> times = plugin.getConfig().getStringList("VCKOH.Times");
@@ -32,5 +59,13 @@ public class GameManager {
 			locations.add(new Location(Bukkit.getWorld(t[0]),Double.parseDouble(t[1]),Double.parseDouble(t[2]),Double.parseDouble(t[3])));
 		}
 		System.out.println("Caching locations...Complete.");
+	}
+	public static void checkTime(){
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+		Date date = new Date();
+		//System.out.println(dateFormat.format(date));
+		if(time.contains(dateFormat.format(date).toString())){
+			System.out.println("Starting game.");
+		}
 	}
 }
--- src/com/SinfulPixel/VCKOH/VCKOH.java
+++ src/com/SinfulPixel/VCKOH/VCKOH.java
@@ -1,18 +1,38 @@
 package com.SinfulPixel.VCKOH;
 
-import com.SinfulPixel.VCKOH.Tasks.Tasks;
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.SinfulPixel.VCKOH.Tasks.GameManager;
+import com.SinfulPixel.VCKOH.Tasks.Tasks;
 
 /**
  * Created by Min3 on 7/21/2014.
  */
 public class VCKOH extends JavaPlugin {
     Tasks t = new Tasks(this);
+    GameManager gm = new GameManager(this);
+    int i = 0;
     public void onEnable(){
+    	try {
+		      saveConfig();
+		      setupConfig(getConfig());
+		      saveConfig(); 
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+    	GameManager.cacheTimes();
+	    GameManager.cacheLoc();
         getServer().getPluginManager().registerEvents(t,this);
+        i = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new BukkitRunnable(){public void run() {GameManager.checkTime();}}, 0L, 60L);
     }
 
     @Override
@@ -24,12 +44,30 @@ public class VCKOH extends JavaPlugin {
                 sender.sendMessage("Creating Point, Check it out!");
             	}else if(args.length==1){
                 	Tasks.regenPoint(Tasks.point.get(true));
+                	
                 	sender.sendMessage("Regenning.");
                 }
             }else{
                 sender.sendMessage("Must be a player to test.");
+                GameManager.checkTime();
             }
         }
         return false;
     }
+    private void setupConfig(FileConfiguration config) throws IOException{
+	    if (!new File(getDataFolder(), "RESET.FILE").exists()) {
+	      new File(getDataFolder(), "RESET.FILE").createNewFile();
+	      config.set("VCKOH.Creator","Min3CraftDud3");
+	      config.set("VCKOH.WebSite","http://www.SinfulPixel.com");
+	      config.set("VCKOH.Capture_Radius_Blocks", 15);
+	      config.set("VCKOH.Capture_Time_Mins", 15);	
+	      config.set("VCKOH.Game_TimeOut_Hrs", 1);
+	      config.set("Times.0","00:00");
+	      config.set("Locations.0.World","world");
+	      config.set("Locations.0.X","100.0");
+	      config.set("Locations.0.Y","100.0");
+	      config.set("Locations.0.Z","100.0");
+	      saveConfig();
+	     }
+	  }
 }
